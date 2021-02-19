eb791ba19ebbd23cca05bd67a68c7f15f7ad49a5
David Pinney <david@pinney.org>
2021-02-19 11:19:44 -0500

    cleaning up old inputs

--- microgridup.py
+++ microgridup.py
@@ -573,70 +573,6 @@ def main(BASE_NAME, LOAD_NAME, REOPT_INPUTS, microgrid, GEN_NAME, FULL_NAME, OMD
 
 if __name__ == '__main__':
 	# Input data.
-	# BASE_NAME = 'lehigh_base_phased.dss'
-	# LOAD_NAME = 'lehigh_load.csv'
-	# REOPT_INPUTS = {
-	# 	"solar" : "on",
-	# 	"wind" : "off",
-	# 	"battery" : "on",
-	# 	"year" : '2017',
-	# 	"energyCost" : "0.12",
-	# 	"demandCost" : '20',
-	# 	"solarCost" : "1600",
-	# 	"windCost" : "4989",
-	# 	"batteryPowerCost" : "840",
-	# 	"batteryCapacityCost" : "420",
-	# 	"dieselGenCost": "500",
-	# 	"solarMin": 0,
-	# 	"windMin": 0,
-	# 	"batteryPowerMin": 0,
-	# 	"batteryCapacityMin": 0,
-	# 	"solarMax": "100000",
-	# 	"windMax": "100000",
-	# 	"batteryPowerMax": "1000000",
-	# 	"batteryCapacityMax": "1000000",
-	# 	"solarExisting": 0,
-	# 	"criticalLoadFactor": "1",
-	# 	"outage_start_hour": "200",
-	# 	"outageDuration": "48",
-	# 	"fuelAvailable": "50000",
-	# 	"genExisting": 0,
-	# 	"minGenLoading": "0.3",
-	# 	"batteryKwExisting": 0,
-	# 	"batteryKwhExisting": 0,
-	# 	"windExisting": 0,
-	# 	"value_of_lost_load": "100",
-	# 	"solarCanCurtail": True,
-	# 	"solarCanExport": True
-	# }
-	# microgrids = {
-	# 	'm1': {
-	# 		'loads': ['634a_data_center','634b_radar','634c_atc_tower'],
-	# 		'switch': '632633',
-	# 		'gen_bus': '634',
-	# 		'gen_obs_existing': ['solar_634_existing']
-	# 	},
-	# 	'm2': {
-	# 		'loads': ['675a_hospital','675b_residential1','675c_residential1'],
-	# 		'switch': '671692',
-	# 		'gen_bus': '675',
-	# 		'gen_obs_existing': []
-	# 	},
-	# 	'm3': {
-	# 		'loads': ['671_command_center','652_residential'],
-	# 		'switch': '671684',
-	# 		'gen_bus': '684',
-	# 		'gen_obs_existing': ['diesel_684_existing']
-	# 	},
-	# 	'm4': {
-	# 		'loads': ['645_hangar','646_office'],
-	# 		'switch': '632645',
-	# 		'gen_bus': '646',
-	# 		'gen_obs_existing': []
-	# 	}
-	# }
-
-	#Second input set.
 	BASE_NAME = 'lehigh_base_phased.dss'
 	LOAD_NAME = 'lehigh_load.csv'
 	REOPT_INPUTS = {
@@ -688,7 +624,6 @@ if __name__ == '__main__':
 	MAP_NAME = 'lehigh_map'
 	REOPT_FOLDER = 'lehigh_reopt'
 	BIG_OUT_NAME = 'output_full_analysis_lehigh.html'
-
 	# 2nd run inputs.
 	microgrid2 = {
 		'loads': ['634a_data_center','634b_radar','634c_atc_tower'],
@@ -696,8 +631,29 @@ if __name__ == '__main__':
 		'gen_bus': '634',
 		'gen_obs_existing': ['solar_634_existing']
 	}
+	# 2nd run outputs.
 	FULL_NAME2 = 'lehigh_fuller.dss'
 	REOPT_FOLDER2 = 'lehigh_reopt_2'
 	BIGGER_OUT_NAME = 'output_full_analysis_lehigh2.html'
+	# other_microgrids = {
+	# 	'm2': {
+	# 		'loads': ['675a_hospital','675b_residential1','675c_residential1'],
+	# 		'switch': '671692',
+	# 		'gen_bus': '675',
+	# 		'gen_obs_existing': []
+	# 	},
+	# 	'm3': {
+	# 		'loads': ['671_command_center','652_residential'],
+	# 		'switch': '671684',
+	# 		'gen_bus': '684',
+	# 		'gen_obs_existing': ['diesel_684_existing']
+	# 	},
+	# 	'm4': {
+	# 		'loads': ['645_hangar','646_office'],
+	# 		'switch': '632645',
+	# 		'gen_bus': '646',
+	# 		'gen_obs_existing': []
+	# 	}
+	# }
 	main(BASE_NAME, LOAD_NAME, REOPT_INPUTS, microgrid, GEN_NAME, FULL_NAME, OMD_NAME, ONELINE_NAME, MAP_NAME, REOPT_FOLDER, BIG_OUT_NAME)
 	main(FULL_NAME, LOAD_NAME, REOPT_INPUTS, microgrid2, GEN_NAME, FULL_NAME2, OMD_NAME, ONELINE_NAME, MAP_NAME, REOPT_FOLDER2, BIGGER_OUT_NAME)

c605d9202a80672135f54da71bab12fcc874526d
David Pinney <david@pinney.org>
2023-06-13 12:29:22 -0400

    workaround for pathing, again

--- tests_backend_full.py
+++ tests_backend_full.py
@@ -6,7 +6,7 @@ import microgridup_gen_mgs as gmg
 
 def test_1mg():
 	# Input data.
-	MODEL_DIR = f'{PROJ_FOLDER}/lehigh_1mg'
+	MODEL_DIR = f'{PROJ_FOLDER}/lehigh1mg'
 	BASE_DSS = f'{MGU_FOLDER}/testfiles/lehigh_base_1mg.dss'
 	LOAD_CSV = f'{MGU_FOLDER}/testfiles/lehigh_load.csv'
 	FAULTED_LINE = '650632' # Why this line, which is not closing off the genbus from source?
@@ -77,7 +77,7 @@ def test_1mg():
 
 def test_2mg():
 	# Input data.
-	MODEL_DIR = f'{PROJ_FOLDER}/lehigh_2mgs'
+	MODEL_DIR = f'{PROJ_FOLDER}/lehigh2mgs'
 	BASE_DSS = f'{MGU_FOLDER}/testfiles/lehigh_base_2mg.dss'
 	LOAD_CSV = f'{MGU_FOLDER}/testfiles/lehigh_load.csv'
 	FAULTED_LINE = '650632'
@@ -154,7 +154,7 @@ def test_2mg():
 
 def test_3mg():
 	# Input data.
-	MODEL_DIR = f'{PROJ_FOLDER}/lehigh_3mgs'
+	MODEL_DIR = f'{PROJ_FOLDER}/lehigh3mgs'
 	BASE_DSS = f'{MGU_FOLDER}/testfiles/lehigh_base_3mg.dss'
 	LOAD_CSV = f'{MGU_FOLDER}/testfiles/lehigh_load.csv'
 	FAULTED_LINE = '650632'
@@ -238,7 +238,7 @@ def test_3mg():
 
 def test_4mg():
 	# Input data.
-	MODEL_DIR = f'{PROJ_FOLDER}/lehigh_4mgs'
+	MODEL_DIR = f'{PROJ_FOLDER}/lehigh4mgs'
 	BASE_DSS = f'{MGU_FOLDER}/testfiles/lehigh_base_4mg.dss'
 	LOAD_CSV = f'{MGU_FOLDER}/testfiles/lehigh_load.csv'
 	FAULTED_LINE = '650632'
@@ -333,7 +333,7 @@ def test_auto3mg():
 	# Input data.
 	CIRC_FILE = f'{MGU_FOLDER}/testfiles/lehigh_base_3mg.dss'
 	CRITICAL_LOADS = ['645_hangar','684_command_center', '611_runway','675a_hospital','634a_data_center', '634b_radar', '634c_atc_tower']
-	MODEL_DIR = f'{PROJ_FOLDER}/lehigh_auto3mg'
+	MODEL_DIR = f'{PROJ_FOLDER}/lehighauto_3mg'
 	BASE_DSS = f'{MGU_FOLDER}/testfiles/lehigh_base_3mg.dss'
 	LOAD_CSV = f'{MGU_FOLDER}/testfiles/lehigh_load.csv'
 	FAULTED_LINE = '670671'
